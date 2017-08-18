e7e7430fef73da35df40c032e0e4ac53a803dcff
= <alla_88@inbox.lv>
2017-08-18 12:11:11 -0300

    task 2 - resize added

--- task2/js/draw.js
+++ task2/js/draw.js
@@ -1,6 +1,5 @@
 'use strict';
-const width = window.innerWidth;
-const height = window.innerHeight;
+var width, height;
 const canvas = document.getElementById('draw');
 const ctx = canvas.getContext('2d');
 const hslMax = 359;
@@ -8,15 +7,28 @@ const hslMin = 0;
 const thickMin = 5;
 const thickMax = 100;
 
-let thick = thickMax-1;
-let color = getRandomInt(hslMin, hslMax);
-canvas.width = width;
-canvas.height = height;
+let thick, color;
+startCanvas();
 
-function getLine(min, max, current, key){
-    if(!key){
-        if(current < max){
-            current ++
+document.addEventListener('mousedown', setLine);
+document.addEventListener('mouseup', stopDraw);
+document.addEventListener('dblclick', clear);
+window.addEventListener('resize', startCanvas);
+
+function startCanvas() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    canvas.width = width;
+    canvas.height = height;
+    clear();
+    thick = thickMax - 1;
+    color = getRandomInt(hslMin, hslMax);
+}
+
+function getLine(min, max, current, key) {
+    if (!key) {
+        if (current < max) {
+            current++
         }
         else {
             current = min;
@@ -24,8 +36,8 @@ function getLine(min, max, current, key){
         return current;
     }
     else {
-        if(current > min){
-            current --
+        if (current > min) {
+            current--
         }
         else {
             current = max;
@@ -36,37 +48,31 @@ function getLine(min, max, current, key){
 }
 
 
-
-
-document.addEventListener('mousedown', setLine);
-document.addEventListener('mouseup', stopDraw);
-document.addEventListener('dblclick', clear);
-
-function getRandomInt(min, max){
+function getRandomInt(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     rand = Math.floor(rand);
     return rand;
 }
 
-function clear(){
+function clear() {
     ctx.clearRect(0, 0, width, height);
 }
 
-function stopDraw(){
+function stopDraw() {
     document.removeEventListener('mousemove', draw);
 }
 
-function setLine(e){
+function setLine(e) {
     ctx.beginPath();
-    ctx.strokeStyle = 'hsl('+ color +',100%,50%)';
+    ctx.strokeStyle = 'hsl(' + color + ',100%,50%)';
     document.addEventListener('mousemove', draw);
     thick = getLine(thickMin, thickMax, thick, e.shiftKey);
-    color =getLine(hslMin, hslMax, color, e.shiftKey);
+    color = getLine(hslMin, hslMax, color, e.shiftKey);
 }
-function draw(e){
+function draw(e) {
     let x = e.offsetX;
     let y = e.offsetY;
-    ctx.lineTo(x,y);
+    ctx.lineTo(x, y);
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
     ctx.lineWidth = thick;
